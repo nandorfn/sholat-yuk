3a03428ddd4d776dbe2fce51f5bc9671842dc7dd
nandorfn <firnandorfn@gmail.com>
2023-04-24 01:24:41 +0700

    add env file

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,17 @@ import CityOption from "@/components/CityOption";
 import Header from "@/components/Header";
 import Body from "@/components/Body";
 
+
 export async function getServerSideProps(context) {
   const { city } = context.query;
-  console.log(city);
-
+  
+  const apiKey = process.env.API_URL
   const today = new Date();
   const year = today.getFullYear();
   const month = String(today.getMonth() + 1).padStart(2, '0');
   const day = String(today.getDate()).padStart(2, '0');
 
-  const allData = await fetch(`https://api.myquran.com/v1/sholat/jadwal/${city}/${year}/${month}/${day}`)
+  const allData = await fetch(`${apiKey}${city}/${year}/${month}/${day}`)
   const rawData = await allData.json()
 
   return {
