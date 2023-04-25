569db6b930eb393e28c902834888f156cc433b34
nandorfn <firnandorfn@gmail.com>
2023-04-25 16:57:56 +0700

    add z-10 to div at Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -20,11 +20,11 @@ export default function Menu({ visible, onClose,jadwal }) {
         <>
             {popUp && (
                 <div className=''>
-                    <div className="bg-white z-10 h-full w-72 absolute transform translate-x-42 transition-transform duration-700">
+                    <div className="bg-white z-20  h-full w-72 absolute">
                         <div><i onClick={handleClick} className="fa-solid fa-circle-xmark absolute fa-xl right-3 top-6"></i></div>
                         <CustomDate jadwal={jadwal}/>
                     </div>
-                    <div onClick={handleClick} className="bg-black h-screen absolute w-screen opacity-50"></div>
+                    <div onClick={handleClick} className="bg-black z-10 h-screen absolute w-screen opacity-50"></div>
                 </div>
             )}
 
